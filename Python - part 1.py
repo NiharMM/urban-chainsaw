74d3eb24db6fd6ee222924d2dab5269366ec4941
NiharMM <113441050+NiharMM@users.noreply.github.com>
2022-09-13 17:59:32 +0530

    Add files via upload

---
+++ Python - part 1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+Radius = float(input("The radius of the circle is: "))
+pi = 3.141592653589793238462643383279502884197
+Area = pi*Radius**2
+print("The area of the circle is", Area)
+
+
+# In[14]:
+
+
+Filename= input("Enter Filename: ")
+
+extension = Filename.split(".")
+
+print ("Extension of the file is : " + extension[-1])
+
+
+# In[ ]:
+
+
+
+
